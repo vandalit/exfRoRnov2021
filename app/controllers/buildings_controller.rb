ddf5959ac9ab0bd58fe6f48f3c04e95c6cd8f2d1
Van <84937597+vandalit@users.noreply.github.com>
2021-11-09 00:06:37 -0300

    modelos y controladores2

---
+++ app/controllers/buildings_controller.rb
@@ -0,0 +1,13 @@
+class BuildingsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
---
+++ test/controllers/buildings_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class BuildingsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get buildings_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get buildings_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get buildings_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get buildings_create_url
+    assert_response :success
+  end
+end
